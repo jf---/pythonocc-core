27bd52815ee3a82275287b544bcc801061cbaba9
Jelle Feringa <jelleferinga@gmail.com>
2015-01-07 10:22:41 +0100

    Utils required for pyrapid support

---
+++ symlink_py27_conda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pyocc_root="/Users/jelleferinga/GIT/pythonocc-core/src/addons"
+
+CONDA=~/miniconda
+PYOCC=$CONDA/lib/python2.7/site-packages/OCC/
+
+cd $PYOCC
+
+rm -rf DataExchange
+rm -rf Utils
+rm -rf Display
+rm -rf KBE
+
+ln -s $pyocc_root/DataExchange $PYOCC/DataExchange
+ln -s $pyocc_root/Utils $PYOCC/Utils
+ln -s $pyocc_root/Display $PYOCC/Display
+ln -s $pyocc_root/KBE $PYOCC/KBE
+
+
+
+
