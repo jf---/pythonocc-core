d45f9d89ebb5aeca9637263eca7afc2d83209ad1
Jelle Feringa <jelleferinga@gmail.com>
2016-04-08 19:21:10 +0200

    wip

--- examples/core_export_stl.py
+++ examples/core_export_stl.py
@@ -19,19 +19,24 @@ import os
 from OCC.StlAPI import StlAPI_Writer
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox
 
-my_box = BRepPrimAPI_MakeBox(10., 20., 30.).Shape()
+def export_stl():
+    my_box = BRepPrimAPI_MakeBox(10., 20., 30.).Shape()
 
-# set the directory where to output the
-directory = os.path.split(__name__)[0]
-stl_output_dir = os.path.abspath(os.path.join(directory, "models"))
+    # set the directory where to output the
+    directory = os.path.split(__name__)[0]
+    stl_output_dir = os.path.abspath(os.path.join(directory, "models"))
 
-# make sure the path exists otherwise OCE get confused
-assert os.path.isdir(stl_output_dir)
-stl_output_file = os.path.join(stl_output_dir, "box.stl")
+    # make sure the path exists otherwise OCE get confused
+    assert os.path.isdir(stl_output_dir)
+    stl_output_file = os.path.join(stl_output_dir, "box.stl")
 
-# its advisable to write binary STL files, this result in a
-# file size reduction of ~10*
-stl_ascii_format = False
+    # its advisable to write binary STL files, this result in a
+    # file size reduction of ~10*
+    stl_ascii_format = False
 
-stl_export = StlAPI_Writer()
-stl_export.Write(my_box, stl_output_file, stl_ascii_format)
+    stl_export = StlAPI_Writer()
+    stl_export.Write(my_box, stl_output_file, stl_ascii_format)
+
+
+if __name__ == "__main__":
+    export_stl()
--- examples/core_load_iges.py
+++ examples/core_load_iges.py
@@ -17,24 +17,35 @@
 
 from __future__ import print_function
 
-import sys
+import os
 
-from OCC.IGESControl import IGESControl_Reader
-from OCC.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
+is_testing = os.getenv("TEST_PYOCC", 1)
+
 from OCC.Display.SimpleGui import init_display
+from OCC.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
+from OCC.IGESControl import IGESControl_Reader
 
-iges_reader = IGESControl_Reader()
-status = iges_reader.ReadFile('./models/surf114.igs')
 
-if status == IFSelect_RetDone:  # check status
+def read_iges():
+    iges_reader = IGESControl_Reader()
+    status = iges_reader.ReadFile('./models/surf114.igs')
+
+    assert status == IFSelect_RetDone  # check status
     failsonly = False
     iges_reader.PrintCheckLoad(failsonly, IFSelect_ItemsByEntity)
     iges_reader.PrintCheckTransfer(failsonly, IFSelect_ItemsByEntity)
-    ok = iges_reader.TransferRoots()
+    iges_reader.TransferRoots()
     aResShape = iges_reader.Shape(1)
-else:
-    print("Error: can't read file.")
-    sys.exit(0)
-display, start_display, add_menu, add_function_to_menu = init_display()
-display.DisplayShape(aResShape, update=True)
-start_display()
+    assert False, "just not right"
+
+    if not is_testing:
+        display, start_display, add_menu, add_function_to_menu = init_display()
+        display.DisplayShape(aResShape, update=True)
+        start_display()
+
+if is_testing:
+    test_read_iges = read_iges
+
+
+if __name__ == "__main__":
+    read_iges()
